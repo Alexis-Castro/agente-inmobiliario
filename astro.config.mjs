e95be9d611e0778a3c6f7dec763a887c8fadbb29
Alexis Castro <castropuse@gmail.com>
2026-05-22 16:14:48 -0500

    remove axios, update package json, astro v2

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,6 +7,7 @@ import tailwind from '@astrojs/tailwind'
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
-  site: 'https://alexis-castro.github.io',
-  base: '/unete'
+
+  // site: 'https://alexis-castro.github.io',
+  // base: '/unete'
 })
